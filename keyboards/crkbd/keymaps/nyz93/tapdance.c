a607af5bd923b82776a711e4408834ded5533798
Zoltan Nyikos <nyikoszoltan0@gmail.com>
2023-12-30 23:36:21 +0100

    fix tapdance

--- keyboards/crkbd/keymaps/nyz93/tapdance.c
+++ keyboards/crkbd/keymaps/nyz93/tapdance.c
@@ -6,7 +6,7 @@ typedef struct {
     uint8_t kc;
 } dance_key_arg;
 
-void dance_key_c_type(qk_tap_dance_state_t* state, void* user_data) {
+void dance_key_c_type(tap_dance_state_t* state, void* user_data) {
     dance_key_arg* arg = (dance_key_arg*)user_data;
     uint8_t        kc  = arg->kc;
     if (state->count > 1) {
@@ -16,7 +16,7 @@ void dance_key_c_type(qk_tap_dance_state_t* state, void* user_data) {
     }
 }
 
-void dance_key_c_done(qk_tap_dance_state_t* state, void* user_data) {
+void dance_key_c_done(tap_dance_state_t* state, void* user_data) {
     dance_key_arg* arg = (dance_key_arg*)user_data;
     uint8_t        kc  = arg->kc;
 
@@ -42,7 +42,7 @@ void dance_key_c_done(qk_tap_dance_state_t* state, void* user_data) {
 
 #define ACTION_TAP_DANCE_HOLD_HOSTMOD(keycode) \
     { .fn = {dance_key_c_type, dance_key_c_done, NULL}, .user_data = (void*)&((dance_key_arg){keycode}) }
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     [KC_TD_C] = ACTION_TAP_DANCE_HOLD_HOSTMOD(KC_C),
     [KC_TD_X] = ACTION_TAP_DANCE_HOLD_HOSTMOD(KC_X),
     [KC_TD_V] = ACTION_TAP_DANCE_HOLD_HOSTMOD(KC_V),
